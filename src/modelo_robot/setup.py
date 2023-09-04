ea52cd4e8c6b3990981ef9c678ec3b5d5d8ddd5c
Andres Cotrino <andres.cotrino@uao.edu.co>
2023-09-03 21:52:46 -0500

    Adding first URDF model of agrobot

---
+++ src/modelo_robot/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+from setuptools import setup
+from setuptools import find_packages
+
+package_name = 'modelo_robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='andres',
+    maintainer_email='andres.cotrino@hotmail.com',
+    description='Visualizacion de robot fumigador',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
