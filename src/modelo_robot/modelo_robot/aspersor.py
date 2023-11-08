478776c8c14d320832535b1f7286e1f2e75b5712
Andres Cotrino <andres.cotrino@uao.edu.co>
2023-11-07 22:45:21 -0500

    Changes

--- src/modelo_robot/modelo_robot/aspersor.py
+++ src/modelo_robot/modelo_robot/aspersor.py
@@ -1,49 +1,84 @@
 #!/usr/bin/env python3
-import time  # Agrega esta línea al comienzo del archivo
-import RPi.GPIO as GPIO
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+import RPi.GPIO as GPIO
+import time
+from sensor_msgs.msg import Joy
 
-class StepperMotorNode(Node):
 
+
+class aspersor(Node): # MODIFY NAME
     def __init__(self):
-        super().__init__('stepper_motor_node')
-        self.publisher_ = self.create_publisher(String, 'stepper_motor_control', 10)
-        self.subscription = self.create_subscription(String, 'stepper_motor_command', self.callback, 10)
-        self.subscription
+        super().__init__("aspersor") # MODIFY NAME
+        self.suscriber_ = self.create_subscription(Joy,"joy",self.callbackJoyPressed,10)
+        self.get_logger().info("Leyendo valores de Joystick")
 
+        self.DIR_PIN = 19  # Pin para la dirección del motor (Dir)
+        self.STEP_PIN = 20  # Pin para el pulso del motor    (STEP)
+        self.bombaPin = 25 # Pin para la bomba
+
+        self.start = 0
+        Joy.buttons
+        
+
+        # Configurar los pines GPIO
         GPIO.setmode(GPIO.BCM)
-        self.DIR_PIN = 19  # Pin para la dirección (DIR)
-        self.STEP_PIN = 20  # Pin para el pulso (STEP)
         GPIO.setup(self.DIR_PIN, GPIO.OUT)
         GPIO.setup(self.STEP_PIN, GPIO.OUT)
-        self.direction = 1  # 1 para un sentido, -1 para el contrario
+        GPIO.setup(self.bombaPin, GPIO.OUT)
 
-    def callback(self, msg):
-        if msg.data == "forward":
-            self.direction = 1
-        elif msg.data == "backward":
-            self.direction = -1
 
-        self.move_motor()
+        
+    def callbackJoyPressed(self,msg):
+        if msg.buttons[2] == 1:
+            self.start += 1
+        if self.start == 2:
+            self.start = 0
+        self.rotarMotor()
 
-    def move_motor(self):
-        steps = 200
-        delay = 0.005
 
-        GPIO.output(self.DIR_PIN, self.direction)
-        for _ in range(steps):
-            GPIO.output(self.STEP_PIN, GPIO.HIGH)
-            time.sleep(delay)  # Utiliza time.sleep() en lugar de rclpy.sleep()
-            GPIO.output(self.STEP_PIN, GPIO.LOW)
-            time.sleep(delay)
+    def aspersar(self):
+        GPIO.output(self.bombaPin, GPIO.HIGH)
+        time.sleep(2)
+        GPIO.output(self.bombaPin, GPIO.LOW)
+
+    def rotarMotor(self):
+        try:
+            
+
+            if self.start == 1:
+
+                # Gira 180 grados hacia la derecha
+                GPIO.output(self.DIR_PIN, GPIO.HIGH)         # Establecer la dirección del motor hacia la derecha
+                for i in range(200):                    # 200->180° y 100->90°
+                    GPIO.output(self.STEP_PIN, GPIO.HIGH)
+                    time.sleep(0.001)                   # Controla la velocidad
+                    GPIO.output(self.STEP_PIN, GPIO.LOW)
+                    time.sleep(0.001)                   # Controla la velocidad
+                    self.aspersar()
+
+                # Gira 180 grados hacia la izquierda
+                GPIO.output(self.DIR_PIN, GPIO.LOW)         # Establecer la dirección del motor hacia la izquierda
+                for i in range(200):                   # 200->180° y 100->90°
+                    GPIO.output(self.STEP_PIN, GPIO.HIGH)
+                    time.sleep(0.001)                  # Controla la velocidad
+                    GPIO.output(self.STEP_PIN, GPIO.LOW)
+                    time.sleep(0.001)                  # Controla la velocidad
+                    self.aspersar()
+                         
+
+        except KeyboardInterrupt:
+            pass
+
+
+
 
 def main(args=None):
     rclpy.init(args=args)
-    node = StepperMotorNode()
+    node = aspersor() # MODIFY NAME
     rclpy.spin(node)
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
