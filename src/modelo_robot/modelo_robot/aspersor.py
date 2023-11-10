07ef0564bdc5957f742baf1878a75f4497a18dcb
Andres Cotrino <andres.cotrino@uao.edu.co>
2023-11-10 17:09:19 -0500

    changes

--- src/modelo_robot/modelo_robot/aspersor.py
+++ src/modelo_robot/modelo_robot/aspersor.py
@@ -50,12 +50,12 @@ class aspersor(Node): # MODIFY NAME
         try:
         
             # Gira el motor 5 revoluciones rápidamente en sentido horario
-            self.step_motor(GPIO.HIGH, 1 * self.stepsPerRevolution, 500)
+            self.step_motor(GPIO.HIGH, 1 * self.stepsPerRevolution, 100000)
             self.aspersar()
             time.sleep(1)
 
             # Gira el motor 5 revoluciones rápidamente en sentido antihorario
-            self.step_motor(GPIO.LOW, 1 * self.stepsPerRevolution, 500)
+            self.step_motor(GPIO.LOW, 1 * self.stepsPerRevolution, 100000)
             self.aspersar()
             time.sleep(1)
 
